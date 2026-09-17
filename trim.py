760b2d93d3fbd2843fb2f03f800b2f6d840b0d63
Adil Arya <SCHOOL_EMAIL>
2026-09-17 14:27:40 -0500

    initializing repo

---
+++ trim.py
@@ -0,0 +1,12 @@
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+
+s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+bucket = "swe-bench-submissions"
+prefix = "verified/20250522_sweagent_claude-4-sonnet-20250514/trajs/"
+for page in s3.get_paginator("list_objects_v2").paginate(Bucket=bucket, Prefix=prefix):
+    for obj in page.get("Contents", []):
+        key = obj["Key"]
+        dest = key[len(prefix):]
+        s3.download_file(bucket, key, f"trajs/{dest}")
